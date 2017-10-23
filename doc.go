e026546b9ccc3168604547756e1bf1b23fd52d83
Olivier Mengué <dolmen@cpan.org>
2017-10-23 23:10:05 +0200

    Add doc

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package flagx provides helpers/extensions for the 'flag' package of the stdlib.
+//
+// Use those helpers with flag.Var() / flag.FlagSet.Var().
+// See https://golang.org/pkg/flag/#Var
+package flagx
