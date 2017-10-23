d64e0034218f7b370fe1462a5c6b744907e5cf3f
Olivier Mengué <dolmen@cpan.org>
2017-10-24 00:14:00 +0200

    flagx NullStr tests: check error output

--- nullstr_test.go
+++ nullstr_test.go
@@ -1,6 +1,7 @@
 package flagx_test
 
 import (
+	"bytes"
 	"flag"
 	"reflect"
 	"testing"
@@ -13,25 +14,31 @@ import (
 type nullStringTester struct {
 	t        *testing.T
 	flagName string
+	output   bytes.Buffer
 }
 
 func (nst *nullStringTester) Check(args []string, expected *string) {
 	var value *string
 	flags := flag.NewFlagSet("TestNullString", flag.ContinueOnError)
+	flags.SetOutput(&nst.output)
 	if args == nil {
 		args = []string{}
 	}
 	flags.Var(flagx.NullString(&value), nst.flagName, "Value")
 	if err := flags.Parse(args); err != nil {
-		nst.t.Fatalf("Unexpected error: %s", err)
+		nst.t.Fatalf("Unexpected error: %s\nError output:\n%s", err, nst.output.String())
 	}
 	if !reflect.DeepEqual(value, expected) {
 		nst.t.Errorf("got %#v expected %#v", value, expected)
 	}
+	if nst.output.Len() > 0 {
+		nst.t.Errorf("Error output:\n%s", nst.output.String())
+		nst.output.Reset()
+	}
 }
 
 func TestNullString(t *testing.T) {
-	tns := nullStringTester{t, "value"}
+	tns := nullStringTester{t: t, flagName: "value"}
 	tns.Check([]string{}, nil)
 	tns.Check([]string{"a"}, nil)
 	tns.Check([]string{"-value", "x"}, ptr.String("x"))
