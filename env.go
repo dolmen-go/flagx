26a6452934d2873c175610857fbfc0305321c46a
Olivier Mengué <dolmen@cpan.org>
2017-10-26 23:05:52 +0200

    Add flagx.Env

---
+++ env.go
@@ -0,0 +1,14 @@
+package flagx
+
+import "os"
+
+// Env wraps a flag.Value and initializes it with the value
+// of the given environment variable if set.
+// The lookup occurs immediately so it happens before command line parsing.
+func Env(key string, v Value) Value {
+	if x, found := os.LookupEnv(key); found {
+		// Ignore errors
+		_ = v.Set(x)
+	}
+	return v
+}
