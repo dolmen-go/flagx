b0276a6c80137a0816466666d933189b8085607d
Olivier Mengué <dolmen@cpan.org>
2017-10-30 23:41:10 +0100

    Refactor Slice/IntSlice tests for more shared code
    Not just copy-paste!

--- slices_test.go
+++ slices_test.go
@@ -8,15 +8,7 @@ import (
 	"github.com/dolmen-go/flagx"
 )
 
-func TestIntSlice(t *testing.T) {
-	tester := varTester{
-		t:        t,
-		flagName: "ints",
-		buildVar: func() (flag.Getter, interface{}) {
-			var value []int
-			return flagx.IntSlice{&value}, &value
-		}}
-
+func checkIntSlice(tester *varTester) {
 	tester.CheckParse([]string{}, ([]int)(nil))
 	tester.CheckParse([]string{"a"}, ([]int)(nil))
 	tester.CheckParse([]string{"-ints", "0"}, []int{0})
@@ -33,8 +25,18 @@ func TestIntSlice(t *testing.T) {
 	tester.CheckHelp()
 }
 
+func TestIntSlice(t *testing.T) {
+	checkIntSlice(&varTester{
+		t:        t,
+		flagName: "ints",
+		buildVar: func() (flag.Getter, interface{}) {
+			var value []int
+			return flagx.IntSlice{&value}, &value
+		}})
+}
+
 func TestSlice(t *testing.T) {
-	tester := varTester{
+	checkIntSlice(&varTester{
 		t:        t,
 		flagName: "ints",
 		buildVar: func() (flag.Getter, interface{}) {
@@ -46,20 +48,5 @@ func TestSlice(t *testing.T) {
 				}
 				return int(n), nil
 			}), &value
-		}}
-
-	tester.CheckParse([]string{}, ([]int)(nil))
-	tester.CheckParse([]string{"a"}, ([]int)(nil))
-	tester.CheckParse([]string{"-ints", "0"}, []int{0})
-	tester.CheckParse([]string{"-ints", "1,2"}, []int{1, 2})
-	tester.CheckParse([]string{"-ints", "2", "-ints", "3"}, []int{2, 3})
-	tester.CheckParse([]string{"-ints", "1,2,3"}, []int{1, 2, 3})
-	tester.CheckParse([]string{"-ints", "1,2,3", "-ints", "4"}, []int{1, 2, 3, 4})
-	tester.CheckParse([]string{"-ints", "1,2,3", "-ints", "4,5"}, []int{1, 2, 3, 4, 5})
-	tester.CheckParse([]string{"-ints", "1,2,3", "-ints", "4,5,6"}, []int{1, 2, 3, 4, 5, 6})
-	tester.CheckParse([]string{"-ints", "0xf,010,-1"}, []int{15, 8, -1})
-	tester.CheckParse([]string{"-ints", "0x7fffffff"}, []int{0x7fffffff})
-	tester.CheckParse([]string{"-ints", "-0x80000000"}, []int{-0x80000000})
-
-	tester.CheckHelp()
+		}})
 }
