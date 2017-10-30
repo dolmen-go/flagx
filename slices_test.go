de265f4d2609e1e9342b4f647307fe8ed52434f5
Olivier Mengué <dolmen@cpan.org>
2017-10-30 23:33:04 +0100

    Add Slice(ptr, separator, parseFunc)
    Similar to IntSlice{} but generic for any slice type. Uses reflect.

--- slices_test.go
+++ slices_test.go
@@ -2,6 +2,7 @@ package flagx_test
 
 import (
 	"flag"
+	"strconv"
 	"testing"
 
 	"github.com/dolmen-go/flagx"
@@ -14,6 +15,37 @@ func TestIntSlice(t *testing.T) {
 		buildVar: func() (flag.Getter, interface{}) {
 			var value []int
 			return flagx.IntSlice{&value}, &value
+		}}
+
+	tester.CheckParse([]string{}, ([]int)(nil))
+	tester.CheckParse([]string{"a"}, ([]int)(nil))
+	tester.CheckParse([]string{"-ints", "0"}, []int{0})
+	tester.CheckParse([]string{"-ints", "1,2"}, []int{1, 2})
+	tester.CheckParse([]string{"-ints", "2", "-ints", "3"}, []int{2, 3})
+	tester.CheckParse([]string{"-ints", "1,2,3"}, []int{1, 2, 3})
+	tester.CheckParse([]string{"-ints", "1,2,3", "-ints", "4"}, []int{1, 2, 3, 4})
+	tester.CheckParse([]string{"-ints", "1,2,3", "-ints", "4,5"}, []int{1, 2, 3, 4, 5})
+	tester.CheckParse([]string{"-ints", "1,2,3", "-ints", "4,5,6"}, []int{1, 2, 3, 4, 5, 6})
+	tester.CheckParse([]string{"-ints", "0xf,010,-1"}, []int{15, 8, -1})
+	tester.CheckParse([]string{"-ints", "0x7fffffff"}, []int{0x7fffffff})
+	tester.CheckParse([]string{"-ints", "-0x80000000"}, []int{-0x80000000})
+
+	tester.CheckHelp()
+}
+
+func TestSlice(t *testing.T) {
+	tester := varTester{
+		t:        t,
+		flagName: "ints",
+		buildVar: func() (flag.Getter, interface{}) {
+			var value []int
+			return flagx.Slice(&value, ",", func(s string) (interface{}, error) {
+				n, err := strconv.ParseInt(s, 0, 0)
+				if err != nil {
+					return nil, nil
+				}
+				return int(n), nil
+			}), &value
 		}}
 
 	tester.CheckParse([]string{}, ([]int)(nil))
