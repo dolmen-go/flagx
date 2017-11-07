ba07d21e2b31425a4f67a17a13fc477ffa4eba98
Olivier Mengué <dolmen@cpan.org>
2017-11-07 09:21:37 +0100

    Doc: add warning about exported but not documented funcs/types

--- doc.go
+++ doc.go
@@ -2,4 +2,9 @@
 //
 // Use those helpers with flag.Var() / flag.FlagSet.Var().
 // See https://golang.org/pkg/flag/#Var
+//
+// Any type/function without documentation is considered work
+// in progress and that part of the API may break. (But note that
+// contributing high quality documentation and increasing code coverage
+// by tests (including runnable examples) is a good way to fix that)
 package flagx
