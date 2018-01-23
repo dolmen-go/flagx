312fb2d1bd1165bf68fcb171cd09d280a69539f1
Olivier Mengué <dolmen@cpan.org>
2018-01-23 10:25:29 +0100

    flagx.Map: pass string values through Set()/UnmarshalText()

--- maps.go
+++ maps.go
@@ -6,21 +6,69 @@ import (
 	"strings"
 )
 
+// Map wraps any map to expose it as a flag.Value.
+//
+// The value type of the map may implement flag.Value. In that case the
+// Set() method will be called on the target element.
+// The element type of the map may implement encoding.TextUnmarshaler. In
+// that case the UnmarshalText() method will be called on the target element.
+//
+// The parse func is optional. If it is set, it must return a value
+// assignable to an element of the slice. If the returned value is a bare
+// string, it will pass through Set() or UnmarshalText() if the type implements
+// it (see above).
 func Map(m interface{}, parseValue func(string) (interface{}, error)) Value {
 	v := reflect.ValueOf(m)
 	if v.IsNil() || v.Kind() != reflect.Map {
 		panic("non-nil pointer to a map expected")
 	}
 	// check that keys are strings
+	// FIXME check must be stricter
 	if v.Type().Key().Kind() != reflect.String {
 		panic("keys must be of type string")
 	}
-	return &stringMap{v, parseValue}
+
+	valueType := v.Type().Elem()
+	set := setterFor(valueType)
+	var buildValue func(value string) (reflect.Value, error)
+	if parseValue == nil {
+		if set == nil {
+			panic(fmt.Errorf("invalid slice type: %s doesn't implement encoding.TextUnmarshaler or flag.Value", v.Type()))
+		}
+		buildValue = func(value string) (reflect.Value, error) {
+			v := reflect.New(valueType).Elem()
+			return v, set(v, value)
+		}
+	} else {
+		if set == nil {
+			buildValue = func(value string) (reflect.Value, error) {
+				v, err := parseValue(value)
+				if err != nil {
+					return reflect.Value{}, err
+				}
+				return reflect.ValueOf(&v).Elem().Elem(), nil
+			}
+		} else {
+			buildValue = func(value string) (reflect.Value, error) {
+				v, err := parseValue(value)
+				if err != nil {
+					return reflect.Value{}, err
+				}
+				if s, isString := v.(string); isString {
+					rv := reflect.New(valueType).Elem()
+					return rv, set(rv, s)
+				}
+				return reflect.ValueOf(&v).Elem().Elem(), nil
+			}
+		}
+	}
+
+	return &stringMap{v, buildValue}
 }
 
 type stringMap struct {
-	Map   reflect.Value
-	Parse func(string) (interface{}, error)
+	Map        reflect.Value
+	buildValue func(string) (reflect.Value, error)
 }
 
 func (m *stringMap) String() string {
@@ -33,17 +81,13 @@ func (m *stringMap) Set(s string) error {
 		return fmt.Errorf("%q: '=' expected", s)
 	}
 	key := s[:i]
-	var value interface{}
-	if m.Parse == nil {
-		value = s[i+1:]
-	} else {
-		var err error
-		value, err = m.Parse(s[i+1:])
-		if err != nil {
-			return err
-		}
+
+	value, err := m.buildValue(s[i+1:])
+	if err != nil {
+		return err
 	}
-	m.Map.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(value))
+
+	m.Map.SetMapIndex(reflect.ValueOf(key), value)
 	return nil
 }
 
