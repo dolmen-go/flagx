b3bbde352a909c835db45b78755d52cd649d0b28
Olivier Mengué <dolmen@cpan.org>
2018-02-04 01:41:54 +0100

    Add Func and BoolFunc

---
+++ func.go
@@ -0,0 +1,31 @@
+package flagx
+
+import (
+	"strconv"
+)
+
+// Func wraps a function like flag.Value.Set() as a flag.Value.
+type Func func(s string) error
+
+func (f Func) Set(s string) error { return f(s) }
+
+func (Func) String() string { return "" }
+
+func (Func) Get() interface{} { return nil }
+
+// BoolFunc wraps a function like flag.Value.Set() as a boolean flag.Value.
+type BoolFunc func(b bool) error
+
+func (BoolFunc) IsBoolFlag() bool { return true }
+
+func (f BoolFunc) Set(s string) error {
+	v, err := strconv.ParseBool(s)
+	if err != nil {
+		return err
+	}
+	return f(v)
+}
+
+func (BoolFunc) String() string { return "" }
+
+func (BoolFunc) Get() interface{} { return nil }
---
+++ func_test.go
@@ -0,0 +1,67 @@
+package flagx_test
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+
+	"github.com/dolmen-go/flagx"
+)
+
+/*
+	defer func(args []string, cmdline *flag.FlagSet) {
+		os.Args = args
+		flag.CommandLine = cmdline
+	}(os.Args, flag.CommandLine)
+*/
+
+func ExampleFunc() {
+	os.Args = []string{os.Args[0], "-push=a", "-push=b"}
+	flag.CommandLine = flag.NewFlagSet("test", flag.PanicOnError)
+
+	var all []string
+
+	// A flag that apppends the given value to slice all
+	flag.Var(
+		flagx.Func(func(s string) error {
+			all = append(all, s)
+			return nil
+		}),
+		"push", "push `value`",
+	)
+
+	flag.Parse()
+
+	fmt.Println(all)
+
+	// Output:
+	// [a b]
+}
+
+func ExampleBoolFunc() {
+	os.Args = []string{os.Args[0], "-rand", "-value=5"}
+	flag.CommandLine = flag.NewFlagSet("test", flag.PanicOnError)
+
+	var n int
+
+	// A flag that set the value of n
+	flag.IntVar(&n, "value", 0, "set given value")
+	// A flag that set n to a random value
+	flag.Var(
+		flagx.BoolFunc(
+			func(b bool) error {
+				n = rand.Int()
+				return nil
+			},
+		),
+		"rand", "set random value",
+	)
+
+	flag.Parse()
+
+	fmt.Println(n)
+
+	// Output:
+	// 5
+}
