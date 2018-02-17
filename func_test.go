d09bad4b33a30f4504101b174b65bba3babee845
Olivier Mengué <dolmen@cpan.org>
2018-02-17 08:45:44 +0100

    Add ExampleFunc_hex: another way for implementing hex encoded flags

--- func_test.go
+++ func_test.go
@@ -1,6 +1,7 @@
 package flagx_test
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"math/rand"
@@ -37,6 +38,31 @@ func ExampleFunc() {
 
 	// Output:
 	// [a b]
+}
+
+// Shows an hex encoded parameter
+func ExampleFunc_hex() {
+	os.Args = []string{os.Args[0], "-hex=68656c6c6f"}
+	flag.CommandLine = flag.NewFlagSet("test", flag.PanicOnError)
+
+	// Destination of the decoded parameter value
+	var bin []byte
+
+	// A flag that apppends the given value to slice all
+	flag.Var(
+		flagx.Func(func(s string) (err error) {
+			bin, err = hex.DecodeString(s)
+			return
+		}),
+		"hex", "hex encoded `value`",
+	)
+
+	flag.Parse()
+
+	fmt.Printf("%q", bin)
+
+	// Output:
+	// "hello"
 }
 
 func ExampleBoolFunc() {
