0f631a62d7bdf9bd4b6fad1eba39f9c824958d5d
Olivier Mengué <dolmen@cpan.org>
2022-09-05 10:48:11 +0200

    doc: add more doc links in package doc

--- doc.go
+++ doc.go
@@ -1,10 +1,9 @@
-// Package flagx provides helpers/extensions for the 'flag' package of the stdlib.
+// Package flagx provides helpers/extensions for the [flag] package of the stdlib.
 //
-// Use those helpers with flag.Var() / flag.FlagSet.Var().
-// See https://golang.org/pkg/flag/#Var
+// Use those helpers with [flag.Var] and [flag.FlagSet.Var].
 //
 // Any type/function without documentation is considered work
 // in progress and that part of the API may break. (But note that
 // contributing high quality documentation and increasing code coverage
-// by tests (including runnable examples) is a good way to fix that)
+// by tests (including runnable examples) is a good way to fix that).
 package flagx
